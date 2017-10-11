0fdae12093ab3760fee348a82087ef9912de77a0
Brittany Walker <breneewalker@gmail.com>
2017-10-11 16:29:41 -0400

    add footer component to homepage

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Dashboard from '../Components/Dashboard';
+import Footer from '../Components/Footer';
 
 class Home extends Component {
   login() {
@@ -8,28 +9,31 @@ class Home extends Component {
   render() {
     const { isAuthenticated } = this.props.auth;
     return (
-      <div className="container">
-        {
-          isAuthenticated() && (
-              <h4>
-                <Dashboard></Dashboard>
-              </h4>
-            )
-        }
-        {
-          !isAuthenticated() && (
-              <h4>
-                This is the homepage. You are not logged in! Please{' '}
-                <a
-                  style={{ cursor: 'pointer' }}
-                  onClick={this.login.bind(this)}
-                >
-                  Log In
-                </a>
-                {' '}to continue.
-              </h4>
-            )
-        }
+      <div>
+        <div className="container">
+          {
+            isAuthenticated() && (
+                <h4>
+                  <Dashboard></Dashboard>
+                </h4>
+              )
+          }
+          {
+            !isAuthenticated() && (
+                <h4>
+                  This is the homepage. You are not logged in! Please{' '}
+                  <a
+                    style={{ cursor: 'pointer' }}
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </a>
+                  {' '}to continue.
+                </h4>
+              )
+          }
+        </div>
+        <Footer></Footer>
       </div>
     );
   }
