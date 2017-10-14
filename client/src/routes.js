cc9f5470417fa7ad9aa067b2e5b6d31b7aab7da9
Brittany Walker <breneewalker@gmail.com>
2017-10-13 23:20:13 -0400

    update react router routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Redirect, Route, Router } from 'react-router-dom';
 import App from './App';
 import Home from './Home/Home';
+import Support from './Components/Support';
+import Dashboard from './Components/Dashboard';
+import Courses from './Components/Courses';
+import Projects from './Components/Projects';
+import Tutorials from './Components/Tutorials';
+import Events from './Components/Events';
+import Resources from './Components/Resources';
 import Profile from './Profile/Profile';
 import Callback from './Callback/Callback';
 import Auth from './Auth/Auth';
@@ -19,19 +26,36 @@ export const makeMainRoutes = () => {
   return (
     <Router history={history} component={App}>
       <div>
-        <Route path="/" render={(props) => <App auth={auth} {...props} />} />
-        <Route path="/" render={(props) => <Home auth={auth} {...props} />} />
-        <Route path="/profile" render={(props) => (
-          !auth.isAuthenticated() ? (
-            <Redirect to="/"/>
-          ) : (
-            <Profile auth={auth} {...props} />
-          )
-        )} />
-        <Route path="/callback" render={(props) => {
-          handleAuthentication(props);
-          return <Callback {...props} /> 
-        }}/>        
+          <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+          <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
+          <Route path="/support" render={(props) => <Support auth={auth} {...props} />} />
+          <Route path="/profile" render={(props) => (
+            !auth.isAuthenticated() ? (
+              <Redirect to="/home"/>
+            ) : (
+              <Profile auth={auth} {...props} />
+            )
+          )} />
+          <Route path="/dashboard" render={(props) => (
+            !auth.isAuthenticated() ? ( <Redirect to="/home"/> ) : ( <Dashboard auth={auth} {...props} /> ))} />
+          
+          <Route path="/resources" render={(props) => (
+            !auth.isAuthenticated() ? ( <Redirect to="/home"/> ) : ( <Resources auth={auth} {...props} /> ))} />          
+
+          <Route path="/courses" render={(props) => (
+            !auth.isAuthenticated() ? ( <Redirect to="/home"/> ) : ( <Courses auth={auth} {...props} /> ))} /> 
+
+          <Route path="/tutorials" render={(props) => (
+            !auth.isAuthenticated() ? ( <Redirect to="/home"/> ) : ( <Tutorials auth={auth} {...props} /> ))} /> 
+
+
+          <Route path="/events" render={(props) => (
+            !auth.isAuthenticated() ? ( <Redirect to="/home"/> ) : ( <Events auth={auth} {...props} /> ))} /> 
+
+          <Route path="/callback" render={(props) => {
+            handleAuthentication(props);
+            return <Callback {...props} /> 
+          }}/>       
       </div>
     </Router>
   );
