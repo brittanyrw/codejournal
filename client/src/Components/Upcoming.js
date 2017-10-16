8f6221c69327767665bb52f831db297556be771f
Brittany Walker <breneewalker@gmail.com>
2017-10-15 22:45:20 -0400

    start styling upcoming features page

--- client/src/Components/Upcoming.js
+++ client/src/Components/Upcoming.js
@@ -12,20 +12,49 @@ class Upcoming extends Component {
 
     const DashContainer = styled.section`
       background: white;
-      height: 100vh;
+      margin-bottom: 80px;
       @media only screen and (min-width: 641px) {
         transition: margin-left .5s;
         margin-left: 100px;
       }
-  `;
+    `;
+
+    const FeaturesWrapper = styled.section`
+      padding: 20px;
+    `;
+
+    const FeatureSquare = styled.div`
+
+    `;
+
+    const FeatureSquareTitle = styled.h1`
+
+    `;    
+
+    const CuratedSquare = styled(FeatureSquare)`
+      
+    `;
 
 
     return (
       <DashContainer>
         <DashNavigation></DashNavigation>
-        <div>Upcoming Features</div>
+        <FeaturesWrapper>
+          <CuratedSquare>
+            <FeatureSquareTitle>Curated Squares</FeatureSquareTitle>
+          </CuratedSquare>                    
+          <FeatureSquare>
+            <FeatureSquareTitle>Notifications</FeatureSquareTitle>
+          </FeatureSquare>
+          <FeatureSquare>
+            <FeatureSquareTitle>Social Media Connections</FeatureSquareTitle>
+          </FeatureSquare>
+          <FeatureSquare>
+            <FeatureSquareTitle>Share Dashboards</FeatureSquareTitle>
+          </FeatureSquare>                    
+        </FeaturesWrapper>
         <DashFooter></DashFooter>
-    </DashContainer>
+      </DashContainer>
     );
   }
 }
